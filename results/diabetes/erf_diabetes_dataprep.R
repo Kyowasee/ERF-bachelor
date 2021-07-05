1c1d05b16358bac83c279e05fa43eb1e48b247f9
Kyowasee <59704194+Kyowasee@users.noreply.github.com>
2021-07-05 13:08:12 +0200

    Add files via upload

---
+++ results/diabetes/erf_diabetes_dataprep.R
@@ -0,0 +1,55 @@
+################################################################################
+#                                                                              #
+#          Training Data Preparation - Pima Indian Diabetes                    #
+#                                                                              #
+################################################################################
+
+
+# Libraries
+# library(mice)
+
+# Function
+
+#' @name prepare_diabetes_data
+#' @description Preparation of the Pima Indians Diabetes data set for ERF
+#' @param data original Pima Indian Diabetes data (see UCI ML Repo)
+#' @param imp_method a single string, or a vector of strings, specifying the imputation method to be used for the attributes 'SkinThickness' and 'Insulin'. If specified as a single string, the same method will be used for all blocks. Common choices: "pmm" = Predictive mean matching, sample" = Random sample from observed values, "cart" =	Classification and regression trees, "rf" = 	Random forest imputations (default).
+#' @return prepared Pima Indian Diabetes data
+
+prepare_diabetes_data <- function(data, imp_method = "rf"){
+  
+  # Shorten some attribute names
+  names(data)[names(data) == "BloodPressure"] <- "BP"
+  names(data)[names(data) == "DiabetesPedigreeFunction"] <- "DPF"
+  
+  
+  # Impute missing data (= 0 except for the attributes 'Pregnancies' & 'Outcome') 
+  cols_missing <- colnames(data)[!colnames(data) %in% c("Pregnancies",
+                                                        "Outcome")]
+  missing <- data[cols_missing] == 0
+  data[cols_missing][missing] <- NA
+  
+  # a) Impute with median where few values are missing per column
+  data$Glucose[is.na(data$Glucose)] <- median(data$Glucose,na.rm = T)
+  data$BP[is.na(data$BP)] <- median(data$BP, na.rm = T)
+  data$BMI[is.na(data$BMI)] <- median(data$BMI,na.rm = T)
+  
+  # b) Predict missing values where many values are missing
+  mice_imp <- mice(data[, c("SkinThickness","Insulin")], method=imp_method, print = F) 
+  mice_complete <- mice::complete(mice_imp)
+  data$SkinThickness <- mice_complete$SkinThickness
+  data$Insulin <- mice_complete$Insulin
+  
+  # turn binary target into factor
+  data$Outcome <- as.factor(data$Outcome)
+  names(data)[names(data) == 'Outcome'] <- 'y'
+  
+  return(data)
+}
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# EXAMPLE
+# data <- read.csv(file = 'diabetes.csv', header = T)
+# data <- prepare_diabetes_data(data)
+# str(data)
